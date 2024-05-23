f6b32b8ee9319f8a9352ddb25a35a8e11e85fe96
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-05-23 12:22:53 +0000

    store & orders commit

--- store/urls.py
+++ store/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.store, name='store'),
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
