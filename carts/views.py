f263ca37cdec560841e1af1fc1f3647dcbaefd59
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-05-24 12:15:19 +0000

    carts & orders modified

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
-from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from django.http import HttpResponse
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -15,10 +15,10 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     current_user = request.user
-    product = Product.objects.get(id=product_id) # get the product
-    # if the user is authenticated
+    product = Product.objects.get(id=product_id) #get the product
+    # If the user is authenticated
     if current_user.is_authenticated:
-        product_variation = [] # get the product variation
+        product_variation = []
         if request.method == 'POST':
             for item in request.POST:
                 key = item
@@ -30,8 +30,9 @@ def add_cart(request, product_id):
                 except:
                     pass
 
+
         is_cart_item_exists = CartItem.objects.filter(product=product, user=current_user).exists()
-        if is_cart_item_exists:    # get the cart item
+        if is_cart_item_exists:
             cart_item = CartItem.objects.filter(product=product, user=current_user)
             ex_var_list = []
             id = []
@@ -39,6 +40,7 @@ def add_cart(request, product_id):
                 existing_variation = item.variations.all()
                 ex_var_list.append(list(existing_variation))
                 id.append(item.id)
+
             if product_variation in ex_var_list:
                 # increase the cart item quantity
                 index = ex_var_list.index(product_variation)
@@ -46,9 +48,9 @@ def add_cart(request, product_id):
                 item = CartItem.objects.get(product=product, id=item_id)
                 item.quantity += 1
                 item.save()
+
             else:
-                # create a new cart item
-                item = CartItem.objects.create(product=product, quantity = 1, user=current_user)
+                item = CartItem.objects.create(product=product, quantity=1, user=current_user)
                 if len(product_variation) > 0:
                     item.variations.clear()
                     item.variations.add(*product_variation)
@@ -64,9 +66,9 @@ def add_cart(request, product_id):
                 cart_item.variations.add(*product_variation)
             cart_item.save()
         return redirect('cart')
-    # if the user is not authenticated
+    # If the user is not authenticated
     else:
-        product_variation = [] # get the product variation
+        product_variation = []
         if request.method == 'POST':
             for item in request.POST:
                 key = item
@@ -78,8 +80,9 @@ def add_cart(request, product_id):
                 except:
                     pass
 
-        try:    # get the cart
-            cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the section
+
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
         except Cart.DoesNotExist:
             cart = Cart.objects.create(
                 cart_id = _cart_id(request)
@@ -87,10 +90,10 @@ def add_cart(request, product_id):
         cart.save()
 
         is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
-        if is_cart_item_exists:    # get the cart item
+        if is_cart_item_exists:
             cart_item = CartItem.objects.filter(product=product, cart=cart)
             # existing_variations -> database
-            # current_variations -> product_variation
+            # current variation -> product_variation
             # item_id -> database
             ex_var_list = []
             id = []
@@ -100,7 +103,7 @@ def add_cart(request, product_id):
                 id.append(item.id)
 
             print(ex_var_list)
-            
+
             if product_variation in ex_var_list:
                 # increase the cart item quantity
                 index = ex_var_list.index(product_variation)
@@ -108,9 +111,9 @@ def add_cart(request, product_id):
                 item = CartItem.objects.get(product=product, id=item_id)
                 item.quantity += 1
                 item.save()
+
             else:
-                # create a new cart item
-                item = CartItem.objects.create(product=product, quantity = 1, cart=cart)
+                item = CartItem.objects.create(product=product, quantity=1, cart=cart)
                 if len(product_variation) > 0:
                     item.variations.clear()
                     item.variations.add(*product_variation)
@@ -127,7 +130,9 @@ def add_cart(request, product_id):
             cart_item.save()
         return redirect('cart')
 
-def remove_cart(request, product_id, cart_item_id ):
+
+def remove_cart(request, product_id, cart_item_id):
+
     product = get_object_or_404(Product, id=product_id)
     try:
         if request.user.is_authenticated:
@@ -143,6 +148,7 @@ def remove_cart(request, product_id, cart_item_id ):
     except:
         pass
     return redirect('cart')
+
 
 def remove_cart_item(request, product_id, cart_item_id):
     product = get_object_or_404(Product, id=product_id)
@@ -169,14 +175,14 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectDoesNotExist: 
-        pass
+    except ObjectDoesNotExist:
+        pass #just ignore
 
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax'       : tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
@@ -197,14 +203,14 @@ def checkout(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectDoesNotExist: 
-        pass # just ignore
+    except ObjectDoesNotExist:
+        pass #just ignore
 
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax'       : tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/checkout.html', context)
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,78 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
-# Create your views here.
+from .models import Order, Payment, OrderProduct
+import json
+from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 
 def payments(request):
-    return render(request, 'orders/payments.html')
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
 
+    # Store transaction details inside Payment model
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    # Move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
+
+
+        # Reduce the quantity of the sold products
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+
+    # Clear cart
+    CartItem.objects.filter(user=request.user).delete()
+
+    # Send order recieved email to customer
+    mail_subject = 'Thank you for your order!'
+    message = render_to_string('orders/order_recieved_email.html', {
+        'user': request.user,
+        'order': order,
+    })
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
+
+    # Send order number and transaction id back to sendData method via JsonResponse
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
@@ -52,7 +116,7 @@ def place_order(request, total=0, quantity=0,):
             dt = int(datetime.date.today().strftime('%d'))
             mt = int(datetime.date.today().strftime('%m'))
             d = datetime.date(yr,mt,dt)
-            current_date = d.strftime("%Y%m%d") #20240422
+            current_date = d.strftime("%Y%m%d") #20210305
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
@@ -68,3 +132,34 @@ def place_order(request, total=0, quantity=0,):
             return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
+
+
+def order_complete(request):
+    return render(request, 'orders/order_complete.html')
+
+
+# def order_complete(request):
+#     order_number = request.GET.get('order_number')
+#     transID = request.GET.get('payment_id')
+
+#     try:
+#         order = Order.objects.get(order_number=order_number, is_ordered=True)
+#         ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+#         subtotal = 0
+#         for i in ordered_products:
+#             subtotal += i.product_price * i.quantity
+
+#         payment = Payment.objects.get(payment_id=transID)
+
+#         context = {
+#             'order': order,
+#             'ordered_products': ordered_products,
+#             'order_number': order.order_number,
+#             'transID': payment.payment_id,
+#             'payment': payment,
+#             'subtotal': subtotal,
+#         }
+#         return render(request, 'orders/order_complete.html', context)
+#     except (Payment.DoesNotExist, Order.DoesNotExist):
+#         return redirect('home')
