5d4eaf2f790b520509e1912f6ecfb2687529989e
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-06-04 22:38:40 +0000

    vincart modified

--- vincart/settings.py
+++ vincart/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get('DATABASES_URL')
+database_url = os.environ.get('DATABASE_URL')
 DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
