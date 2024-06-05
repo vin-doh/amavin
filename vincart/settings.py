43be87f4e5975a1500198e3e896e73e87d504fa6
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-06-05 00:13:40 +0000

    vincart modified

--- vincart/settings.py
+++ vincart/settings.py
@@ -160,11 +160,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'vincart', 'static'),
+    os.path.join(BASE_DIR, 'static/')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
