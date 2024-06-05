152f1462bc9260b5cff2ee4ff8652ab69e0df5e8
Vincent Doh <99025815+vin-doh@users.noreply.github.com>
2024-06-05 01:22:05 +0000

    vincart modified

--- vincart/settings.py
+++ vincart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'false'
 # DEBUG = os.environ.get('DEBUG', 'False').lower() == 'false'
 
 # ALLOWED_HOSTS = []
